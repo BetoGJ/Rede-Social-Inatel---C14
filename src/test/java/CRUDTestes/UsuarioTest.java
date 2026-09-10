291ebfd8b6c5158982570eb5d0b8dc42e0761ae3
dudu4325 <luiseduardomarques60@gmail.com>
2026-09-10 03:13:39 -0300

    test: Adicionando testes do método alterarSenha de Usuario

---
+++ src/test/java/CRUDTestes/UsuarioTest.java
@@ -0,0 +1,40 @@
+package CRUDTestes;
+
+import crud.Usuario;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsuarioTest {
+    private Usuario usuario;
+    private final String senha = "Chizinho67!";
+
+    @BeforeEach
+    public void setup(){
+        String email = "christopher@inatel.br";
+        this.usuario = new Usuario(email, senha);
+    }
+
+    @Test
+    public void testeAlterarSenha(){
+        String senhaNovaValida = "Theofficetop1!";
+        boolean troca = usuario.alterarSenha(senha, senhaNovaValida);
+
+        assertTrue(troca);
+    }
+
+    @Test
+    public void testeAlterarSenhaIgual(){
+        boolean troca = usuario.alterarSenha(senha, senha);
+
+        assertFalse(troca);
+    }
+
+    @Test
+    public void testeAlterarSenhaInvalida(){
+        String senhaInvalida = "321";
+        boolean troca = usuario.alterarSenha(senha, senhaInvalida);
+
+        assertFalse(troca);
+    }
+}
