c234a4f1f422be9921031e646c4f303cef179959
dudu4325 <luiseduardomarques60@gmail.com>
2026-09-10 02:43:57 -0300

    feat: Criando a classe Usuario e seus métodos

---
+++ src/main/java/crud/Usuario.java
@@ -0,0 +1,41 @@
+package crud;
+
+public class Usuario {
+    private final String email;
+    private String senhaUsuario;
+    private boolean logado;
+
+    public Usuario(String email, String senha){
+        this.email = Login.verificarEmail(email);
+        this.senhaUsuario = Login.verificarSenha(senha);
+    }
+
+    public boolean alterarSenha(String senhaVerificadora, String senhaNova){
+        if (!senhaVerificadora.equals(senhaUsuario)){
+            return false;
+        }
+
+        if (senhaNova.equals(senhaUsuario)){
+            return false;
+        }
+
+        if (!senhaNova.equals(Login.verificarSenha(senhaNova))){
+            return false;
+        }
+
+        this.senhaUsuario = senhaNova;
+        return true;
+    }
+
+    public void setLogado(boolean logado) {
+        this.logado = logado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean getLogado() {
+        return logado;
+    }
+}
