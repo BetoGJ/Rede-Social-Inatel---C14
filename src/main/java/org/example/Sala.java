e0a0717f349b62a63b14a96a43545731d232c964
thiagov09 <thiago.v0089@gmail.com>
2026-09-10 19:37:41 -0300

    feat: add TestSala.java MockUsuario.java

--- src/main/java/org/example/Sala.java
+++ src/main/java/org/example/Sala.java
@@ -1,14 +1,16 @@
 package org.example;
 
 import crud.Usuario;
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class Sala {
     private int capacidadeMaxima;
     private int quantidadeUsuarios;
     private static int quantidadeSalas;
     private ArrayList<Usuario> usuariosAtivos = new ArrayList<>();
-    public void Sala(capacidadeMaxima){
+
+
+    public Sala(int capacidadeMaxima){
         quantidadeUsuarios=0;
         quantidadeSalas+=1;
         this.capacidadeMaxima = capacidadeMaxima;
@@ -18,5 +20,27 @@ public class Sala {
             quantidadeUsuarios += 1;
         }
         usuariosAtivos.add(usuarioNovo);
+    }
+    public void removeUsuario(Usuario usuarioRemover){
+        if(quantidadeUsuarios>0){
+            quantidadeUsuarios-=1;
+            usuariosAtivos.remove(usuarioRemover);
+        }
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public int getQuantidadeUsuarios() {
+        return quantidadeUsuarios;
+    }
+
+    public static int getQuantidadeSalas() {
+        return quantidadeSalas;
+    }
+
+    public ArrayList<Usuario> getUsuariosAtivos() {
+        return usuariosAtivos;
     }
 }
---
+++ src/test/java/Chat/TestSala.java
@@ -0,0 +1,35 @@
+package Chat;
+
+import crud.Usuario;
+import org.example.Sala;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestSala {
+    @Test
+    public void salaVazia(){
+        Sala salaTeste = new Sala(12);
+        assertTrue(salaTeste.getQuantidadeUsuarios()==0);
+    }
+    @Test
+    public void salaVagasDisponiveis(){
+        Sala salaTeste = new Sala(12);
+        assertTrue(salaTeste.getQuantidadeUsuarios()<salaTeste.getCapacidadeMaxima());
+    }
+    @Test
+    public void AddUsuario(){
+        Sala salaTeste = new Sala(12);
+        Usuario usuario = new MockUsuario("teste@email.com", "teste");
+        salaTeste.addUsuario(usuario);
+        assertTrue(salaTeste.getQuantidadeUsuarios()==1);
+    }
+    @Test
+    public void RemoveUsuario(){
+        Sala salaTeste = new Sala(12);
+        Usuario usuario = new MockUsuario("teste@email.com", "teste");
+        salaTeste.addUsuario(usuario);
+        salaTeste.removeUsuario(usuario);
+        assertTrue(salaTeste.getQuantidadeUsuarios()==0);
+    }
+
+}
